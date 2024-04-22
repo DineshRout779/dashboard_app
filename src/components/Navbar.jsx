ea6a8167161c50df702dbb2c1c88d59cafb74c8a
Dinesh Rout <rddinesh779@gmail.com>
2024-04-22 11:50:12 +0530

    docs: updated docs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { User } from 'phosphor-react';
 
 const Navbar = () => {
   return (
-    <header className='w-full sticky top-0 bg-white flex gap-4 justify-end items-end p-4 border-b border-gray-300'>
+    <header className='w-full sticky top-0 z-10 bg-white flex gap-4 justify-end items-end p-4 border-b border-gray-300'>
       <button className='bg-blue-700 text-white p-2 px-4 rounded-md'>
         Create
       </button>
