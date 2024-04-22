db43b92f53eefd0e9c09fff806ca4343005eaa59
Dinesh Rout <rddinesh779@gmail.com>
2024-04-22 11:23:38 +0530

    added nav and sidebar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { User } from 'phosphor-react';
+
+const Navbar = () => {
+  return (
+    <header className='w-full sticky top-0 bg-white flex gap-4 justify-end items-end p-4 border-b border-gray-300'>
+      <button className='bg-blue-700 text-white p-2 px-4 rounded-md'>
+        Create
+      </button>
+      <button className='bg-gray-300 text-white p-2 rounded-md'>
+        <User size={24} weight='fill' className='text-gray-500' />
+      </button>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,49 @@
+import { House, FilmSlate, ChartBar, GridFour } from 'phosphor-react';
+import amazonLogo from '../assets/amazon.png';
+
+const Sidebar = () => {
+  return (
+    <div className='bg-white sticky top-0 h-screen border-r border-gray-300'>
+      <a
+        href=''
+        className='block font-bold text-xl text-center p-[22px] px-8 border-b border-gray-300'
+      >
+        P
+      </a>
+      <div className='p-4 md:px-6 space-y-4'>
+        <a
+          href=''
+          className='block hover:bg-blue-100 hover:text-blue-700 rounded-md p-2'
+        >
+          <House size={28} />
+        </a>
+        <a
+          href=''
+          className='block hover:bg-blue-100 hover:text-blue-700 rounded-md p-2'
+        >
+          <img src={amazonLogo} alt='' className='w-8' />
+        </a>
+        <a
+          href=''
+          className='block hover:bg-blue-100 hover:text-blue-700 rounded-md p-2'
+        >
+          <FilmSlate size={28} />
+        </a>
+        <a
+          href=''
+          className='block hover:bg-blue-100 hover:text-blue-700 rounded-md p-2'
+        >
+          <GridFour size={28} />
+        </a>
+        <a
+          href=''
+          className='block hover:bg-blue-100 hover:text-blue-700 rounded-md p-2'
+        >
+          <ChartBar size={28} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
