0ccda5871fb0368389a367a78158236c55cfaa27
Dinesh Rout <rddinesh779@gmail.com>
2024-04-22 11:23:53 +0530

    added content header

---
+++ src/components/Content.jsx
@@ -0,0 +1,24 @@
+import Navbar from './Navbar';
+import QuickCreate from './QuickCreate';
+import Templates from './Templates';
+import Usage from './Usage';
+import YourGallery from './YourGallery';
+
+const Content = () => {
+  return (
+    <div className='w-full'>
+      <Navbar />
+
+      <div className='p-5 md:p-8 bg-gray-100 min-h-[90vh] '>
+        <div className='grid gap-4 grid-cols-1 md:grid-cols-3 mb-8 auto-rows-max'>
+          <QuickCreate />
+          <YourGallery />
+          <Usage />
+        </div>
+        <Templates />
+      </div>
+    </div>
+  );
+};
+
+export default Content;
